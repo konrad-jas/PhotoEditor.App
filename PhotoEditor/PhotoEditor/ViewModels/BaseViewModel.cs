fdb29df5b1724b0cb59e803fc95dc2a55bb0798b
Konrad Jasiński <konrjas@gmail.com>
2017-01-12 20:21:56 +0100

    Base viewmodel

---
+++ PhotoEditor/PhotoEditor/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoEditor.ViewModels
+{
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public bool Busy { get; private set; }
+		private readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+
+		public async void RunInBackground<TArg>(Func<Task<TArg>> backgroundTask, Action<TArg> callbackAction = null)
+		{
+			await Task.Run(() => _resetEvent.WaitOne());
+			Busy = true;
+			var result = await Task.Run(async () => await backgroundTask());
+			if (result != null)
+				callbackAction?.Invoke(result);
+
+			Busy = false;
+			_resetEvent.Set();
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+		{
+			var memberExpr = propertyExpression.Body as MemberExpression;
+			var memberName = memberExpr?.Member.Name;
+			if(memberName != null)
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+		}
+	}
+}
